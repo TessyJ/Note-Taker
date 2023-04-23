9022103e83877646842a99b1553ebda201162973
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-04-23 19:55:59 -0400

    Added routes list

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const PORT = process.env.PORT || 3000;
 // Static middleware pointing to the public folder
 app.use(express.static("public"));
 
+// Routes list
+app.get("/index", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+// End Routes List 
+
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
