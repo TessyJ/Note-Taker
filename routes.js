888c9dc5ec20006178384810a0411efeab8335ab
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-04-23 20:27:28 -0400

    Post Request and move items to top

--- routes.js
+++ routes.js
@@ -8,9 +8,29 @@ const router = require("express").Router();
 router.get('/api/notes', async (req,res) => {
 
     let data = await fs.readFileSync('./db/db.json','utf-8');
-    console.log(data)
+    res.status(200).json(JSON.parse(data))
+})
 
-    res.status(200).json({data})
+//Add Notes to existing note
+router.post('/api/notes', async (req,res) =>{
+    let newNote = { ...req.body, id : uniqId() }
+
+    //read existing data and push new note to array
+    let data = await fs.readFileSync("./db/db.json", "utf8");
+    const dataJson = JSON.parse(data);
+    dataJson.unshift(newNote);
+
+    //save new note back to db.json
+    await fs.writeFile("./db/db.json", JSON.stringify(dataJson), (err, note) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Success, added a new note");
+      res.json(data);
+    });
+
+
 })
 
 module.exports = router
